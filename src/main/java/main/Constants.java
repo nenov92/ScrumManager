131e5fbee6224386e579014ab6685584445666be
Miroslav Nenov <m.nenov92@gmail.com>
2016-03-10 21:16:09 +0000

    flow.jpg and norms.conf located with path on server; norms.conf relocated

--- src/main/java/main/Constants.java
+++ src/main/java/main/Constants.java
@@ -3,7 +3,10 @@ package main;
 public class Constants {
 	public static int TIME = 0;
 	public static final int SLEEP_TIME = 2500;
-	public static final String NORMS_FILE = "norms.conf";
-	public static final String NORMS_FILE_ON_SERVER = "F:/Dev/Java Workspace/scrum-workflow/norms.conf";
-	public static final String GUI_BG_FILE_ON_SERVER = "F:/Dev/Java Workspace/scrum-workflow/src/main/resources/flow.jpg";
+	
+	// Workaround to locate files when residing on server
+	private static String locationOnServer = Constants.class.getProtectionDomain().getCodeSource().getLocation().getFile().toString();
+	public static final String NORMS_FILE_ON_SERVER = locationOnServer.replaceAll("main/Constants.class", "norms.conf");
+	public static final String GUI_BG_FILE_ON_SERVER = locationOnServer.replaceAll("main/Constants.class", "flow.jpg");
+
 }
--- src/main/java/main/ScrumWorkflowApplication.java
+++ src/main/java/main/ScrumWorkflowApplication.java
@@ -3,7 +3,6 @@ package main;
 import static org.camunda.bpm.engine.variable.Variables.createVariables;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import main.gui.Console;
 import main.gui.GUI;
@@ -12,16 +11,10 @@ import main.gui.Helper;
 import org.camunda.bpm.application.PostDeploy;
 import org.camunda.bpm.application.ProcessApplication;
 import org.camunda.bpm.application.impl.ServletProcessApplication;
-import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.authorization.Groups;
-import org.camunda.bpm.engine.identity.Group;
-import org.camunda.bpm.engine.identity.User;
 
 @ProcessApplication("Scrum Workflow App")
 public class ScrumWorkflowApplication extends ServletProcessApplication {
-
-	private final Logger LOGGER = Logger.getLogger(ScrumWorkflowApplication.class.getName());
 
 	@PostDeploy
 	public void startProcess(ProcessEngine engine) throws IOException, InterruptedException {
@@ -34,49 +27,11 @@ public class ScrumWorkflowApplication extends ServletProcessApplication {
 		new Helper(new GUI(40, 77));
 
 		Thread.sleep(Constants.SLEEP_TIME);
-		
+
 		main.Helper.runThreads();
 
 		engine.getRuntimeService().startProcessInstanceByKey("scrum-workflow",
 				createVariables().putValue("continue", true).putValue("implement", false));
 	}
 
-	@SuppressWarnings("unused")
-	private void startProcessInstance(ProcessEngine engine) {
-		engine.getRuntimeService().startProcessInstanceByKey("scrum-workflow",
-				createVariables().putValue("continue", true).putValue("implement", false));
-	}
-
-	@SuppressWarnings("unused")
-	private void createUsers(ProcessEngine engine) {
-		final IdentityService identityService = engine.getIdentityService();
-
-		if (identityService.isReadOnly()) {
-			LOGGER.info("Identity service provider is Read Only, not creating any demo users.");
-			return;
-		}
-
-		User singleResult = identityService.createUserQuery().userId("po").singleResult();
-		if (singleResult != null) {
-			return;
-		}
-
-		LOGGER.info("Generating demo data for invoice showcase");
-
-		User user = identityService.newUser("po");
-		user.setFirstName("Product");
-		user.setLastName("Owner");
-		user.setPassword("po");
-		identityService.saveUser(user);
-
-		// create group
-		if (identityService.createGroupQuery().groupId(Groups.CAMUNDA_ADMIN).count() == 0) {
-			Group camundaAdminGroup = identityService.newGroup(Groups.CAMUNDA_ADMIN);
-			camundaAdminGroup.setName("camunda BPM Administrators");
-			camundaAdminGroup.setType(Groups.GROUP_TYPE_SYSTEM);
-
-			identityService.saveGroup(camundaAdminGroup);
-		}
-
-	}
 }
